5657ca2b342490d60f8808cf096ec6cb15c709eb
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2023-04-22 02:21:07 +0700

    fix: change function name

--- recommender_api/api.py
+++ recommender_api/api.py
@@ -11,6 +11,6 @@ app = flask.Flask(__name__)
 @app.route('/api/recommendplace/', methods = ['POST'])
 def api_recommendplace():
     req_body = flask.request.get_json()
-    recommended_places = PlaceRecommender().recommend(req_body['features'], req_body['top_n'])
+    recommended_places = PlaceRecommender().recommend_attraction(req_body['features'], req_body['top_n'])
 
     return json.dumps({"recommended_places":recommended_places.tolist()})

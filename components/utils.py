56dfb10dd200eee4c2166e495b09318e42b5d623
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2023-04-22 02:16:46 +0700

    feat: add utility function

--- components/utils.py
+++ components/utils.py
@@ -18,3 +18,5 @@ def str_to_datetime(instr):
 def str_to_time(instr):
         dt = datetime.strptime(instr, TIME_FORMAT)
         return time(dt.hour, dt.minute, dt.second)
+def row_to_dict(row):
+    return row.to_dict()

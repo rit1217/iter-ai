1fd73bd535c9d93aaa27f2cd37ad61e99d691735
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2023-05-01 13:24:27 +0700

    feat: add restaurant recommender function

--- recommender/place_recommender.py
+++ recommender/place_recommender.py
@@ -20,11 +20,11 @@ class PlaceRecommender:
         
         return items_list[['place_id', 'similarity_score']].sort_values('similarity_score', ascending=False)
     
-    def _roulette_selector(self, places):
+    def _roulette_selector(self, places, size=15):
         pop_fitness = places['popularity'].sum()
         places['probability'] = places.popularity / pop_fitness
-
-        return np.random.choice(places['place_id'], p=places['probability'])
+        # print(places.probability)
+        return np.random.choice(places['place_id'], size=size, p=places['probability'], replace=False)
     
     def recommend_attraction(self, features, activities, destination, top_n=15):
         # df = pd.read_csv(DATA_FILEPATHS['place_with_type'])
@@ -63,3 +63,37 @@ class PlaceRecommender:
         df_candidates = df_merged[df_merged.place_id.isin(candidates_id)]
 
         return df_candidates[columns[:len(columns)-1]][:top_n]
+
+    def recommend_restaurant(self, cuisine_types, destination, top_n):
+        num_dist = top_n // len(cuisine_types)
+        candidates_id = []
+        columns = ['place_id', 'place_name', 'cuisine_types', 'category_code', 'latitude', 'longitude', 'opening_hours', 'popularity']
+
+        df_place = pd.read_csv(DATA_FILEPATHS['place'])
+        df_ophr = pd.read_csv(DATA_FILEPATHS['opening_hour'])
+        df_place_pop = pd.read_csv(DATA_FILEPATHS['place_popularity'])
+        df_cuisine_types = pd.read_csv(DATA_FILEPATHS['cuisine_type'])
+        
+        df_place = df_place[df_place.category_code == "RESTAURANT"]
+        df_cuisine_types = df_cuisine_types.groupby(['place_id']).agg({'description': list}).reset_index()
+        grouped = df_ophr.groupby(['place_id']).apply(lambda x: x[['day', 'opening_time', 'closing_time']].apply(row_to_dict, axis=1).tolist()).reset_index(name='opening_hours')
+
+        df_merged = pd.merge(df_place, df_cuisine_types, how="left", on="place_id")
+        df_merged = pd.merge(df_merged, grouped, on='place_id', how='left')
+        df_merged = pd.merge(df_merged, df_place_pop, how="left", on=["place_id", "place_name", "destination"])
+        df_merged.rename(columns={'description': 'cuisine_types'}, inplace=True)
+
+        for cuisine in cuisine_types:
+            mask = (df_merged[~df_merged.cuisine_types.isna()].
+                    cuisine_types.apply(lambda row: cuisine in row))
+            temp_df = df_merged[~df_merged.cuisine_types.isna()]
+            candidates_id.append(self._roulette_selector(temp_df[mask].copy(), size=num_dist))
+            
+        candidates_id = np.concatenate(candidates_id)
+
+        if len(candidates_id) < top_n:
+            candidates_id = np.concatenate((candidates_id, self._roulette_selector(df_merged[~df_merged.cuisine_types.isna()].copy(), size=top_n-len(candidates_id))))
+
+        df_candidates = df_merged[df_merged.place_id.isin(candidates_id)]
+        return df_candidates[columns[:len(columns)-1]][:top_n]
+            

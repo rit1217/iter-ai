9b755730d18de7137c2a623922a18958a1e6265f
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2023-05-17 00:47:35 +0700

    fix: remove wrong merge code

--- components/utils.py
+++ components/utils.py
@@ -57,9 +57,6 @@ def nearest_place(lst1, lst2):
             best_dist = dist
 
     return nearest_place
-    
-    dt = datetime.strptime(instr, TIME_FORMAT)
-    return time(dt.hour, dt.minute, dt.second)
 
 def row_to_dict(row):
     return row.to_dict()
